59847d126e22d49aa786c24924886b4060faef4c
CharlotteFallices <zhuyuhang_school@163.com>
2022-05-11 17:06:55 +0800

    Add resign script

---
+++ Resign.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+func shell(_ command: String) -> String {
+    let task = Process()
+    let pipe = Pipe()
+    
+    task.standardOutput = pipe
+    task.standardError = pipe
+    task.arguments = ["-c", command]
+    task.launchPath = "/bin/zsh"
+    task.standardInput = nil
+    task.launch()
+    
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: .utf8)!
+    
+    return output
+}
+
+if shell("codesign")=="zsh: command not found: codesign"
+    shell("xcode-select --install")
+shell("codesign --sign - --force --deep ./WeChat.app")
